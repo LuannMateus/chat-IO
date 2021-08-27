5825cfa20d1913da7622492e2adfd50e361aedef
LuannMateus <luanmateuslima34@gmail.com>
2021-08-26 23:23:58 -0300

    Improve the Button, Chat and Header components

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const Header = () => {
   return (
     <header id="header">
-      <h1 className="header__title">Chat IO</h1>
+      <Link to="/" className="header__title">
+        <h1 className="header__title">Chat IO</h1>
+      </Link>
     </header>
   );
 };
